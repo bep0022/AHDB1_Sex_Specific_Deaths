aea96452da0adc2741006f93353bbaa75a1fd800
Ali Amer <ali.amer1421@gmail.com>
2025-12-07 23:25:47 -0600

    Update AHDB_MS1_Data_PreProcessing.R

--- AHDB_MS1_Data_PreProcessing.R
+++ AHDB_MS1_Data_PreProcessing.R
@@ -81,3 +81,195 @@ AHDB_Exp1_MS1 <- AHDB_Exp1_Final %>%
 write.csv(AHDB_Exp1_MS1, 
           file = "AHDB_Exp1_MS1.csv", 
           row.names = FALSE)
+
+### Data Processing & Filtering - qPCR Measures of mtDNA and Telomeres
+# Import raw qPCR output for each plate 1) the Single Copy Autosomal Gene (EEF2) and mtDNA gene multiplex and 2) the Telomere reaction. 
+
+SCNAG <- read.csv("AHDB1_Manuscript1_Deaths_Multiplex_2025-08-01 13-38-10_795BR20744 -  Quantification Cq Results.csv")
+dim(SCNAG)
+
+Telo <- read.csv("AHDB1_Manuscript1_Deaths_Telomere_Heidinger_2025-08-01 11-52-51_795BR20744 -  Quantification Cq Results.csv")
+dim(Telo)
+```
+
+# Edit 
+
+# Concatenate data across runs for the same samples
+Plate1 <- rbind(SCNAG, Telo)
+dim(Plate1)
+
+# Add a column called PlateID and fill in correct Plate number to use as a variable in the statistics
+Plate1$PlateID <- "Plate1"
+
+# CHECK AND EDIT FOR YOUR DATA. 
+#Name Correct Targets based on the fluorphores used in your reaction.
+Plate1$Target[Plate1$Fluor == "VIC"] <- "scnag"
+Plate1$Target[Plate1$Fluor == "FAM"] <- "mtdna"
+Plate1$Target[Plate1$Fluor == "SYBR"] <- "telomeres"
+```
+
+# Identify and remove outliers (across the three replicates)
+
+# Add a column called "Diff_AVG-Cq"
+Plate1$Diff_AVG_Cq <- abs(Plate1$Cq - Plate1$Cq.Mean)
+
+## Add a column called "Flag_outlier"
+Plate1$Flag_outlier <- ifelse(Plate1$Diff_AVG_Cq > 0.4001, "yes", "no")
+HighCq <- Plate1[Plate1$Diff_AVG_Cq > "0.3001", ]
+VeryHighCq <- Plate1[Plate1$Diff_AVG_Cq > "0.4001", ]
+
+# Report number of rows to be removed
+print(paste("Number of rows with High Cq:", nrow(VeryHighCq)))
+# Make a table called  of the high cq values
+HighCq_Samples<- HighCq[c("Well", "Sample", "Fluor", "Diff_AVG_Cq" )]
+print(HighCq_Samples)
+
+
+# Filter and remove rows where Diff_AVG_Cq > 0.4001 for each SampleID
+# USed to be max(AVG) but error said that col did not exist
+dim(Plate1)
+Plate1 <- Plate1 %>%
+  group_by(Sample, Target) %>%
+  filter(!(Diff_AVG_Cq > 0.4001 & Diff_AVG_Cq == max(Diff_AVG_Cq)))
+dim(Plate1)
+
+```
+
+# Additional identification and removal of outliers
+
+# Recalculate Cq Mean and Add a column called "Cq Mean2"
+Plate1 <- Plate1 %>%
+  group_by(Sample, Target) %>%
+  mutate(
+    Cq.Mean2 = mean(Cq),
+    SQ.Mean2 = mean(Starting.Quantity..SQ.)) %>%
+  ungroup()
+dim(Plate1)
+
+# Add a column called "Diff_AVG-Cq_2" that contains the difference between new Cq and new mean
+Plate1$Diff_AVG_Cq_2 <- abs(Plate1$Cq - Plate1$Cq.Mean2)
+
+# Add a column called "Flag_outlier_2"
+Plate1$Flag_outlier_2 <- ifelse(Plate1$Diff_AVG_Cq_2 > 0.401, "yes", "no")
+VeryHighCq_2 <- Plate1[Plate1$Diff_AVG_Cq > "0.401", ]
+# Report number of rows to be removed
+print(paste("Number of rows with High Cq:", nrow(VeryHighCq_2)))
+# Make a table called  of the high cq values
+HighCq_2_Samples<- HighCq[c("Well", "Sample", "Fluor", "Diff_AVG_Cq" )]
+print(HighCq_2_Samples)
+
+# Remove the rows with a high Cq Outliers
+# USed to be max(AVG) but error said that col did not exist
+
+Plate1 <- Plate1 %>%
+  group_by(Sample, Target) %>%
+  filter(!(Diff_AVG_Cq_2 > 0.4001 & Diff_AVG_Cq_2 == max(Diff_AVG_Cq_2)))
+
+dim(Plate1)
+
+###############  If any "Samples" do not have more than two rows (replicates left), remove
+Plate1 <- Plate1 %>%
+  group_by(Sample, Target) %>%
+  filter(n() >= 2) %>%
+  ungroup()
+dim(Plate1)
+
+
+# Remove negative controls and standards
+
+# rows that have "NEG", "POS" in column "Sample" and remove rows with "STD" in Sample "Content"
+Plate1 <- Plate1 %>%
+  filter(!str_detect(Content, "Std-*")) %>%
+  filter(!str_detect(Content, "NTC")) %>%
+  filter(!str_detect(Sample, "Neg")) %>%
+  filter(!str_detect(Sample, "STD*"))
+dim(Plate1)
+
+
+# Subset dataset based on the value in "Target" column
+
+unique_targets <- unique(Plate1$Target)
+# Create a list to store the subset dataframes
+subset_dfs <- list()
+# Loop through each unique value in 'Target', subset the dataframe, and store in subset_dfs
+for (target_value in unique_targets) {
+  subset_df <- subset(Plate1, Target == target_value)
+  subset_dfs[[target_value]] <- subset_df
+}
+
+
+# Now subset_dfs is a list where each element is a dataframe containing rows for each unique 'Target' value
+
+Plate1_SCNAG<-print(subset_dfs[["scnag"]])
+Plate1_SCNAG<-Plate1_SCNAG[ ,c("PlateID", "Well", "Sample", "Target", "Cq", "Cq.Mean",  "Flag_outlier", "SQ.Mean")]
+Plate1_SCNAG <- Plate1_SCNAG %>% 
+  rename(Target_SCNAG = Target, Cq_SCNAG = Cq, Cq.Mean_SCNAG = Cq.Mean, Flag_outlier_SCNAG=Flag_outlier, SQ.Mean_SCNAG=SQ.Mean)
+
+Plate1_mtDNA<-print(subset_dfs[["mtdna"]])
+Plate1_mtDNA<-Plate1_mtDNA[ ,c("PlateID", "Well", "Sample", "Target", "Cq", "Cq.Mean", "Flag_outlier", "SQ.Mean")]
+Plate1_mtDNA <- Plate1_mtDNA %>% 
+  rename(Target_mtDNA = Target, Cq_mtDNA = Cq, Cq.Mean_mtDNA = Cq.Mean, Flag_outlier_mtDNA = Flag_outlier, SQ.Mean_mtDNA = SQ.Mean)
+
+Plate1_Telomeres<-print(subset_dfs[["telomeres"]])
+Plate1_Telomeres<-Plate1_Telomeres[ ,c("PlateID", "Well", "Sample", "Target", "Cq", "Cq.Mean", "Flag_outlier", "SQ.Mean")]
+Plate1_Telomeres <- Plate1_Telomeres %>% 
+  rename(Cq_Telomeres = Cq, Cq.Mean_Telomeres = Cq.Mean, Flag_outlier_Telomeres = Flag_outlier, SQ.Mean_Telomeres = SQ.Mean)
+
+
+#  Make a final MPX dataset for by merging the Target datasets horizontally, in rows.
+
+Plate1_FinalMPX <- merge(Plate1_SCNAG, Plate1_mtDNA, by = c("PlateID", "Well", "Sample"))
+
+# Normalize mtDNA
+# Add a column called mtDNA, and calculate the normalized value 
+Plate1_FinalMPX$mtDNA <- (Plate1_FinalMPX$SQ.Mean_mtDNA / Plate1_FinalMPX$SQ.Mean_SCNAG)
+
+# Recalculate mean across the replicates
+Plate1_FinalMPX <- Plate1_FinalMPX %>%
+  group_by(Sample) %>%
+  mutate(
+    mtDNA.Mean = mean(mtDNA)) %>%
+  ungroup()
+
+
+# Merge final MPX with Telomeres
+
+## Reduce datasets to single row per individual containing only the columns we want.
+Plate1_FinalMPX <- distinct(Plate1_FinalMPX, PlateID, Sample, SQ.Mean_SCNAG, Cq.Mean_SCNAG, SQ.Mean_mtDNA, mtDNA.Mean)
+Plate1_FinalTelo <- distinct(Plate1_Telomeres, PlateID, Sample, SQ.Mean_Telomeres, Cq.Mean_Telomeres)
+
+## Merge the files horizontally
+Plate1_FinalData <- merge(Plate1_FinalMPX, Plate1_FinalTelo, by = c("PlateID", "Sample"))
+
+# Normalize Telomeres
+Plate1_FinalData <- Plate1_FinalData %>% mutate(Telomeres.per.cell = SQ.Mean_Telomeres / SQ.Mean_SCNAG)
+
+
+# Aggregate to get one row per sample, taking mean of normalized mtDNA and telomeres
+Plate1_FinalData <- Plate1_FinalData %>%
+  group_by(Sample) %>%
+  summarize(
+    SQ.Mean_SCNAG = mean(SQ.Mean_SCNAG),
+    SQ.Mean_mtDNA = mean(SQ.Mean_mtDNA),
+    mtDNA.Mean = mean(mtDNA.Mean),
+    Cq.Mean_SCNAG = mean(Cq.Mean_SCNAG),
+    SQ.Mean_Telomeres = mean(SQ.Mean_Telomeres),
+    Cq.Mean_Telomeres = mean(Cq.Mean_Telomeres),
+    Telomeres.per.cell = mean(Telomeres.per.cell)
+  ) %>%
+  ungroup()
+
+
+# Merge Final Data with Trait MetaData for your individuals
+# Load in Data
+Trait <- read.csv("Trait_MetaData.csv")
+dim (Trait)
+
+# Merge both datasets
+FinalData <- merge(Plate1_FinalData, Trait, by = c("Sample"))
+
+
+
+# Write the final data file for this plate
+write.csv(file = "AHDB1_Manuscript1_ESEBqPCR_FinalData.csv", FinalData, row.names = FALSE)
+
